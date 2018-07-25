71e132175e714845f63363ee6653eced32b4e026
Chris James <chris@webglowit.net>
2018-07-25 18:36:25 +1000

    fix: gtmId should initialise as null

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import TagManager from "./TagManager";
 import { TagManagerConfig } from './types'
 
-const install = function(Vue, initConf: TagManagerConfig = { gtmId : '' }) {
+const install = function(Vue, initConf: TagManagerConfig = { gtmId : null }) {
     TagManager.initialize(initConf)
 }
 
