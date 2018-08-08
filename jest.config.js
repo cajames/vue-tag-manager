96a022e9688b6f7bbf87cc1951b6835d9b30068a
Chris James <chris@webglowit.net>
2018-08-08 17:50:56 +1000

    chore: fix coverage

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-	transform: {
-		"^.+\\.tsx?$": "ts-jest"
-	},
-	testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-	collectCoverage: true,
-	moduleNameMapper: {
-		"@/(.*)": "src/$1"
-	}
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+  moduleNameMapper: {
+    "@/(.*)": "src/$1"
+  }
 };
