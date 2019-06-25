0120234adee736de8390bf98f724799ce2bbf8db
Chris James <chris@webglowit.net>
2019-06-25 15:38:35 +1000

    chore: updated dependencies (#9)
    - Handle major changes to JSDom in 11.12 ([read here](https://github.com/facebook/jest/issues/6766))
    - Setting test URL for jest environment to handle above ([read here](https://jestjs.io/docs/en/configuration#testurl-string))

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   collectCoverage: true,
+  testURL: "http://localhost/",
   collectCoverageFrom: [
     "src/**/*.ts",
     "!**/*.d.ts",
